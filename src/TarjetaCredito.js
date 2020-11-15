04d000bd072807a3d29f7158304434a85acfadc0
JJimenez-1 <joselito0310@gmail.com>
2020-11-15 23:05:05 +0100

    feat(TarjetaCredito): Constructor TarjetaCredito y funcion pagar

--- src/TarjetaCredito.js
+++ src/TarjetaCredito.js
@@ -1,1 +1,19 @@
-va
+var owner = "";
+var number = "";
+var credit = 3000;
+var symbol = "EZI";
+
+function TarjetaCredito(owner, number){
+    this.owner = owner;
+    this.number = number;
+}
+
+function pay(charge) {
+    if (charge <= this.credit) {
+        this.credit -= charge;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
