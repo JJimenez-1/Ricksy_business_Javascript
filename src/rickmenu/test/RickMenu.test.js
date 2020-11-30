129a66f2395a122a4aa23e70bc2b903b9442c162
JJimenez-1 <joselito0310@gmail.com>
2020-11-30 18:10:43 +0100

    feat(package.json): Añadir package.json para funcionar test unit

---
+++ src/rickmenu/test/RickMenu.test.js
@@ -0,0 +1,48 @@
+const CreditCard = require('../../tarjetacredito/tarjetacredito');
+const RickMenu = require('../rickmenu');
+const { test, expect } = require('@jest/globals');
+
+beforeEach(() => {
+    morty = new CreditCard("Morty", 46578912);
+    menu = new RickMenu();
+});
+
+test('Comprobar menu no es null', () => {
+    expect(menu).not.toBeNull();
+});
+
+test('Pagar 1 menu', () => {
+    menu.dispatch(morty);
+    expect(morty.credit).toBe(2990);
+});
+
+test('No hay stock', () => {
+    noStock = new CreditCard("CompraTodo", 3);
+    for (let i = 0; i < 101; i++) {
+        menu.dispatch(noStock);
+    }
+    menu.dispatch(morty);
+    expect(morty.credit).toBe(3000);
+    expect(menu.stock).toBe(0);
+});
+
+test('Morty no puede pagar', () => {
+    menu1 = new RickMenu();
+    menu2 = new RickMenu();
+    menu3 = new RickMenu();
+    for (let i = 0; i < 101; i++) {
+        menu1.dispatch(morty);
+        menu2.dispatch(morty);
+        menu3.dispatch(morty);
+    }
+    menu.dispatch(morty);
+    expect(menu.stock).toBe(100)
+})
+
+test('Pagar 100 menu', () => {
+    for (let i = 0; i < 101; i++) {
+        menu.dispatch(morty);
+    }
+    expect(morty.credit).toBe(2000);
+});
+
