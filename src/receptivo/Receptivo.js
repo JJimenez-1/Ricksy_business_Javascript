36fe466e24f9faf8c5bd0727f4e6e8c0c76a8366
JJimenez-1 <joselito0310@gmail.com>
2020-12-07 17:47:06 +0100

    feat(Receptivo): Añadir nuevo modulo Receptivo

--- src/receptivo/Receptivo.js
+++ src/receptivo/Receptivo.js
@@ -3,5 +3,29 @@ function Receptivo() {
     this.observers = new Set();
 }
 
-Receptivo.prototype.dispatch = function (observer ) {
+Receptivo.prototype.registra = function (observer) {
+    this.observers.add(observer);
 }
+
+Receptivo.prototype.dispatch = function (propietario) {
+    for (let observer of this.observers) {
+        observer.dispatch(propietario);
+    }
+}
+
+const singletonReceptivo = (function() {
+    var instance;
+    function createInstance() {
+      return new Receptivo()
+    }
+    return {
+      getReceptivo: function() {
+        if(!instance) {
+          instance = createInstance();
+        }
+        return instance;
+      },
+    };
+  })();
+
+module.exports = singletonReceptivo;
