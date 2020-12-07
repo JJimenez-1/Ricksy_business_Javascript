351d3d89cafc0a7086e67d896cda9df4c78aac1a
JJimenez-1 <joselito0310@gmail.com>
2020-12-07 18:43:14 +0100

    feat(Main): Añadir codigo para el main RicskyBusiness

--- src/RicksyBusiness.js
+++ src/RicksyBusiness.js
@@ -2,6 +2,8 @@
 const CrystalExpender = require("./crystalexpender/CrystalExpender.js");
 var tarjetaCredito =  require("./tarjetacredito/TarjetaCredito.js");
 var singletonUfosPark =  require("./ufospark/UfosPark.js");
+var singletonReceptivo = require("./receptivo/Receptivo.js");
+const RickMenu = require("./rickmenu/rickmenu.js");
 
     /**
     * Crea una tarjeta de crédito para Abradolph.
@@ -90,120 +92,47 @@ var singletonUfosPark =  require("./ufospark/UfosPark.js");
     packExpender.dispatch(gearHead);
     console.log("Packs\n" + packExpender);
     console.log("Credito de GearHead: " + gearHead.credit);
-    /*
-    var ufos= new UfosPark();
 
-    console.log(ufos.flota);
-    // Da de alta en la flota de ovnis 2 UFOS.
-        
-    // Procesamos el pago y reserva de ovni de Abradolph
-
-    // Mostramos el ID del ovni asignado a Abradolph
-    console.log("\nOvni de Abradolph\n" + 
-                        "=================");
-    console.log(ufosPark.getUfoOf(abradolph.number()));
-       
-    // Mostramos el credito de la tarjeta de Abradolph
-    console.log("Credito de Abradolph: " + abradolph.credit());
-
-    // La dualidad en Abradolph quiere reservar otro ovni.
-    // El sistema detecta que ya tiene uno 
-    // e ignora la petición.
-
-    console.log("\nAbradolph quiere otro ovni\n" + 
-                        "==========================");
-    console.log("Su credito no ha cambiado: " + abradolph.credit());
-    console.log("Ovni de Abradolph: " + ufosPark.getUfoOf(abradolph.number()));
-
-    // A GearHead le vacía la tarjeta el alien "Cámara Lenta" 
-    // mientras le daba la chapa, justo antes de pagar el ovni.
-    // Intenta reservarlo y el componente de reserva de ovnis
-    // no le asigna ninguno.
-
-    console.log("\nLLega GearHead!\n" + 
-                        "===============");
-
-        // le vacían la cartera
-
-    console.log("Su credito es cero: " + gearHead.credit());
-    console.log("No puede reservar ovni: " + ufosPark.getUfoOf(gearHead.number()));
-        
-    // Squanchy deja su ovni reservado
-    // antes de irse a squanchear
+    let receptivo = singletonReceptivo.getReceptivo();
+    receptivo.registra(packExpender);
+    receptivo.registra(ufosPark);
 
     console.log("\nLLega Squanchy!\n" + 
-                             "==============");
-    console.log("Su credito es: " + squanchy.credit());
-    console.log("Su ovni es: " + ufosPark.getUfoOf(squanchy.number()));
-
-    // Morty quiere un ovni para huir de la fiesta
-    // pero ya no queda ninguno disponible
-
-    console.log("\nAlgun ovni para Morty?\n" + 
-                        "======================");
-    console.log("Su credito no ha cambiado: " + morty.credit());
-    console.log("No hay ovni Morty: " + ufosPark.getUfoOf(morty.number()));
-
-    // Metemos un ovni más en la flota de ovnis
-    // y mostramos la flota por consola
-
-    console.log("\nFlota de ovnis\n" + 
-                        "==============");
-    console.log(ufosPark);
-
-    // Muestra el total de packs y su precio unidad
-    console.log("\nPacks\n" + 
-                        "=====");
-    console.log(packExpender);
-
-    // Abradolph compra su pack de bienvenida
-
-    console.log("\nAbradolph compra su pack\n" + 
-                        "========================");
-    console.log("Packs\n" + packExpender);
-    console.log("Credito de Abradolph: " + abradolph.credit());
-
-    // El pobre GerHead no tiene crédito para comprar su pack
-    console.log("\nGearHead sin credito para su pack\n" + 
-                        "=================================");
-    console.log("Packs\n" + packExpender);
-    console.log("Credito de GearHead: " + gearHead.credit());
-
-    // Squanchy reserva ovni (ya tiene) y pack
-
-    console.log("\nLLega Squanchy!\n" + 
-                        "===============");
-
-    // Gearhead reserva ovni y pack.
-    // No tiene crédito.
-
-    console.log("\nLLega GearHead!\n" + 
-                             "===============");
-
-    // Birdpearson es recibido en la fiesta
+    "===============");
+    receptivo.dispatch(squanchy);
+    mostrarReserva(gearHead, packExpender, ufosPark);
 
     console.log("\nLLega Birdpearson!\n" + 
-                        "==================");
-
-    // Morty intenta reserver un ovni y un pack pero no quedan
+    "==================");
+    let birdpearson = new tarjetaCredito("Birdpearson", "1111111111111111");
+    receptivo.dispatch(birdpearson);
+    mostrarReserva(birdpearson, packExpender, ufosPark);
 
     console.log("\nMorty quiere pack y ovni pero no quedan :(\n" + 
-                        "==========================================");
+    "==========================================");
+    receptivo.dispatch(morty);
+    mostrarReserva(morty, packExpender, ufosPark);
 
+    let MenuDispatcher = new RickMenu();
+    receptivo.registra(MenuDispatcher);
 
-    console.log("\nPedidos de RickMenus:\n" + 
-                        "=====================");
+    let cards = [abradolph, squanchy, morty, gearHead, birdpearson];
+
+    for (let card of cards) {
+        receptivo.dispatch(card);
+    }
+    
+    console.log("\n" + "Pedidos de RickMenus:");
     console.log(MenuDispatcher);
+    console.log("\n" + "Creditos de los invitados/as:");
 
-    console.log("\nCreditos de los invitados/as:\n" + 
-                        "=============================");
-
+    for(let card of cards) {
         console.log(card);
         console.log();
+    }
 
-
-   
-    console.log(card);
-    console.log("Packs: " + expender.stock());
-    console.log("Ovni: " + ufos.getUfoOf(card.number()));
-*/
+    function mostrarReserva(card, expender, ufos) {
+        console.log(card.number);
+        console.log("Packs: " + expender.stock);
+        console.log("Ovni: " + ufos.getUfoOf(card.number));
+    }
